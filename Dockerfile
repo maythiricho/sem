45ff7bbe25ce55402d869f79b8c1b7e7773e4797
maythiricho <maythiricho172@gmail.com>
2025-09-29 14:14:43 +0630

    testing docker-compose.yml

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM openjdk:18
-WORKDIR /app
-COPY target/DevOpsLab1-0.1.0.2-jar-with-dependencies.jar app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"]
+
+WORKDIR /tmp
+
+# Make sure to include the .jar extension
+COPY ./target/DevOpsLab1-0.1.0.2-jar-with-dependencies.jar /tmp
+
+ENTRYPOINT ["java", "-jar", "DevOpsLab1-0.1.0.2-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -1,38 +1,34 @@
 package com.napier.sem;
 
-public class Employee {
-    /**
-     * Employee number
+/* Represents an employee
+ */
+public class Employee
+{
+    /* Employee number
      */
     public int emp_no;
 
-    /**
-     * Employee's first name
-     */
+    /* Employee's first name
+        */
     public String first_name;
 
-    /**
-     * Employee's last name
-     */
+    /* Employee's last name
+        */
     public String last_name;
 
-    /**
-     * Employee's job title
-     */
+    /* Employee's job title
+        */
     public String title;
 
-    /**
-     * Employee's salary
-     */
+    /* Employee's salary
+        */
     public int salary;
 
-    /**
-     * Employee's current department
-     */
+    /* Employee's current department
+        */
     public String dept_name;
 
-    /**
-     * Employee's manager
-     */
+    /* Employee's manager
+        */
     public String manager;
 }
