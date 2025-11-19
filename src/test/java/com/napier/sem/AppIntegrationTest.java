4e81a6634c69dbbbbc312069bfd91549e74ef3e4
maythiricho <maythiricho172@gmail.com>
2025-11-19 20:46:43 +0630

    Testing codcov

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.napier.sem;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 public class AppIntegrationTest
 {
     static App app;
@@ -11,31 +15,16 @@ public class AppIntegrationTest
     static void init()
     {
         app = new App();
-        // local docker MySQL container ကို 33060 ပေါ် run လိုက်တယ် assumption
         app.connect("localhost:33060", 30000);
+
     }
 
     @Test
     void testGetEmployee()
     {
         Employee emp = app.getEmployee(255530);
-        assertEquals(255530, emp.emp_no);
-        assertEquals("Ronghao", emp.first_name);
-        assertEquals("Garigliano", emp.last_name);
-    }
-
-    @Test
-    void testAddEmployee()
-    {
-        Employee emp = new Employee();
-        emp.emp_no = 500000;
-        emp.first_name = "Kevin";
-        emp.last_name = "Chalmers";
-        app.addEmployee(emp);
-
-        Employee fetched = app.getEmployee(500000);
-        assertEquals(500000, fetched.emp_no);
-        assertEquals("Kevin", fetched.first_name);
-        assertEquals("Chalmers", fetched.last_name);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
     }
 }
